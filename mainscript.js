c9bbf0435b7a8c18658edce31aaab25e8323615f
adam <19adamchristie96@gmail.com>
2023-07-02 16:11:20 +0100

    Create function which checks which cell ID has been clicked on the gameboard and matches the ID to the corresponding index in the array

--- mainscript.js
+++ mainscript.js
@@ -7,10 +7,46 @@ const gameBoard = (function(){
         }
     }
     
-    const addMove = function(){
-        movesArray[3] = 'X';
+    const addMove = function(currentArrayPos){
+        movesArray[currentArrayPos] = 'X';
         render();
     }
+
+    const checkCell = function(currentCellID){
+        if(currentCellID === "child-cell0"){
+            let currentArrayPos = 0
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell1"){
+            let currentArrayPos = 1
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell2"){
+            let currentArrayPos = 2
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell3"){
+            let currentArrayPos = 3
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell4"){
+            let currentArrayPos = 4
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell5"){
+            let currentArrayPos = 5
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell6"){
+            let currentArrayPos = 6
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell7"){
+            let currentArrayPos = 7
+            addMove(currentArrayPos);
+        } else if(currentCellID === "child-cell8"){
+            let currentArrayPos = 8
+            addMove(currentArrayPos);
+        }
+    }
+
+    document.querySelector('.game-board').addEventListener('click', function(e){
+        let currentCellID = e.target.id;
+        checkCell(currentCellID);
+    })
 
     return{
         render,
@@ -18,4 +54,3 @@ const gameBoard = (function(){
     }
 })();
 
-gameBoard.render();
